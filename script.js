bae9713cf90f6244c4867ec66699ab5f71d37271
tobiasz-rayski <124359533+tobiasz-rayski@users.noreply.github.com>
2023-05-14 23:53:53 +0200

    Write instructions to plan out

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-let operations = {
-  divide: {},
-};
+// Declare string variable (display)
+// Declare two variables for calculations
+// Declare object with booleans (operators)
+//
+// Number pressed
+//    add value to string (display) variable
+//    update display
+//
+// Operator pressed
+//    reset booleans
+//    add string value to a variable for calculations
+//    reset string (display) value
+//    switch boolean value
+//
+// Number pressed
+//
+// ...
+//
+// Equals pressed
+//    add current string (display) value to second variable (for calculations)
+//    check for boolean, if true then run appropriate func
+//    add result to string (display) and update display (round to .##)
